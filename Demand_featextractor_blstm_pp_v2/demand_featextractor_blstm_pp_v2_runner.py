2d25dea398aa8ad63b1bbac1476850c37984f477
ken-nag <jimijeffericking@icloud.com>
2020-07-23 20:11:32 +0900

    feat: featextracto_blstm_v2

---
+++ Demand_featextractor_blstm_pp_v2/demand_featextractor_blstm_pp_v2_runner.py
@@ -0,0 +1,147 @@
+import torch
+import sys
+import time
+sys.path.append('../')
+from models.featextractor_blstm_pp_v2 import FeatExtractorBlstm_pp_v2
+from data_utils.voice_demand_dataset import VoicebankDemandDataset
+from data_utils.data_loader import FastDataLoader
+from utils.loss import PSA
+from utils.visualizer import show_TF_domein_result
+import numpy as np
+from utils.stft_module import STFTModule
+from utils.common_utils import EarlyStopping
+import torchaudio.functional as taF
+
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+class FeatExtractorBlstm_pp_v2_Runner():
+    def __init__(self, cfg):
+        
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.dtype= torch.float32
+        self.eps = 1e-8
+        
+        self.stft_module = STFTModule(cfg['stft_params'], self.device)
+        self.stft_module_ex1 = STFTModule(cfg['stft_params_ex1'], self.device)
+        self.stft_module_ex2 = STFTModule(cfg['stft_params_ex2'], self.device)
+        
+        self.train_data_num = cfg['train_data_num']
+        self.valid_data_num = cfg['valid_data_num']
+        self.sample_len = cfg['sample_len']
+        self.epoch_num = cfg['epoch_num']
+        self.train_batch_size = cfg['train_batch_size']
+        self.valid_batch_size = cfg['valid_batch_size']
+        
+        self.train_full_data_num = cfg['train_full_data_num']
+        self.valid_full_data_num = cfg['valid_full_data_num']
+        self.save_path = cfg['save_path']
+        
+        self.train_dataset = VoicebankDemandDataset(data_num=self.train_data_num, 
+                                                    full_data_num=self.train_full_data_num,
+                                                    sample_len=self.sample_len, 
+                                                    folder_type='train')
+        
+        self.valid_dataset = VoicebankDemandDataset(data_num=self.valid_data_num, 
+                                                    full_data_num=self.valid_full_data_num,
+                                                    sample_len=self.sample_len, 
+                                                    folder_type='validation')
+        
+        self.train_data_loader = FastDataLoader(self.train_dataset, 
+                                                batch_size=self.train_batch_size, 
+                                                shuffle=True)
+        
+        self.valid_data_loader = FastDataLoader(self.valid_dataset, 
+                                                batch_size=self.valid_batch_size, 
+                                                shuffle=True)
+      
+        self.model = FeatExtractorBlstm_pp_v2(cfg['dnn_cfg']).to(self.device)
+        self.criterion = PSA()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
+        self.early_stopping = EarlyStopping(patience=10)
+        
+    def _preprocess(self, noisy, clean):
+        with torch.no_grad():
+            noisy_spec = self.stft_module.stft(noisy, pad=False)
+            noisy_amp_spec = taF.complex_norm(noisy_spec)
+            noisy_mag_spec = self.stft_module.to_normalize_mag(noisy_amp_spec)
+            
+            clean_spec = self.stft_module.stft(clean, pad=False)
+            clean_amp_spec = taF.complex_norm(clean_spec)
+            
+            #ex1
+            ex1_noisy_spec = self.stft_module_ex1.stft(noisy, pad=False)
+            ex1_noisy_amp_spec = taF.complex_norm(ex1_noisy_spec)
+            ex1_noisy_mag_spec = self.stft_module_ex1.to_normalize_mag(ex1_noisy_amp_spec)
+            #ex2
+            ex2_noisy_spec = self.stft_module_ex2.stft(noisy, pad=False)
+            ex2_noisy_amp_spec = taF.complex_norm(ex2_noisy_spec)
+            ex2_noisy_mag_spec = self.stft_module_ex2.to_normalize_mag(ex2_noisy_amp_spec)
+            
+            return noisy_mag_spec, ex1_noisy_mag_spec, ex2_noisy_mag_spec, clean_amp_spec, noisy_amp_spec, noisy_spec, clean_spec
+        
+    def _run(self, mode=None, data_loader=None):
+        running_loss = 0
+        for i, (noisy, clean) in enumerate(data_loader):
+            noisy = noisy.to(self.dtype).to(self.device)
+            clean = clean.to(self.dtype).to(self.device)
+            noisy_mag_spec, ex1_noisy_mag_spec, ex2_noisy_mag_spec, clean_amp_spec,  noisy_amp_spec, noisy_spec, clean_spec = self._preprocess(noisy, clean)
+            
+            self.model.zero_grad()  
+            est_mask = self.model(noisy_mag_spec, ex1_noisy_mag_spec, ex2_noisy_mag_spec)
+            est_source = noisy_spec * est_mask[...,None]
+            
+            if mode == 'train' or mode == 'validation':
+                loss = self.criterion(est_source, clean_spec)
+                running_loss += loss.data
+                if mode == 'train':
+                    loss.backward()
+                    self.optimizer.step()
+            
+        return (running_loss / (i+1)), est_source, est_mask, noisy_amp_spec, clean_amp_spec
+    
+    def train(self):
+        train_loss = np.array([])
+        valid_loss = np.array([])
+        print("start train")
+        for epoch in range(self.epoch_num):
+            # train
+            print('epoch{0}'.format(epoch))
+            start = time.time()
+            self.model.train()
+            tmp_train_loss, _, _, _, _ = self._run(mode='train', data_loader=self.train_data_loader)
+            train_loss = np.append(train_loss, 
+                                   tmp_train_loss.cpu().clone().numpy())
+            
+            self.model.eval()
+            with torch.no_grad():
+                tmp_valid_loss, est_source, est_mask, noisy_amp_spec, clean_amp_spec = self._run(mode='validation', data_loader=self.valid_data_loader)
+            
+                # if self.early_stopping.validation(tmp_valid_loss):
+                #     torch.save(self.model.state_dict(), self.save_path + 'u_net{0}.ckpt'.format(epoch + 1))
+                #     break
+                
+                valid_loss = np.append(valid_loss, 
+                                       tmp_valid_loss.cpu().clone().numpy())
+                
+            if (epoch + 1) % 10 == 0:
+                plot_time = time.time()
+                est_source = taF.complex_norm(est_source)
+                show_TF_domein_result(train_loss, 
+                                      valid_loss, 
+                                      noisy_amp_spec[0,:,:],
+                                      est_mask[0,:,:],
+                                      est_source[0,:,:],
+                                      clean_amp_spec[0,:,:])
+                print('plot_time:', time.time() - plot_time)
+                torch.save(self.model.state_dict(), self.save_path + 'u_net{0}.ckpt'.format(epoch + 1))
+            
+            end = time.time()
+            print('----excute time: {0}'.format(end - start))
+           
+                        
+if __name__ == '__main__':
+    from configs.demand_featextractor_blstm_pp_v2_config_1 import train_cfg
+    obj = FeatExtractorBlstm_pp_v2_Runner(train_cfg)
+    obj.train()
---
+++ models/featextractor_blstm_pp_v2.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+class FeatExtractorBlstm_pp_v2(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.f_size = cfg['f_size']
+        
+        self.kernel=cfg['kernel']
+        self.stride=cfg['stride']
+        self.channel=cfg['channel']
+        self.dilation=cfg['dilation']
+
+        self.mix_kernel=cfg['mix_kernel']
+        self.mix_stride=cfg['mix_stride']
+        self.mix_channel=cfg['mix_channel']
+        self.mix_dilation=cfg['mix_dilation']
+        
+        self.first_linear_out=cfg['first_linear_out']
+        self.hidden_size = cfg['hidden_size']
+        
+        self.encoder = self._encoder(channels=self.channel, kernel_size=self.kernel, stride=self.stride, dilation=self.dilation)
+        self.mix_encoder = self._encoder(channels=self.mix_channel, kernel_size=self.mix_stride, stride=self.mix_stride, dilation=self.mix_dilation)
+        self.compressor = self._encoder(channels=(self.mix_channel[1],1), kernel_size=(1,1), stride=(1,1))
+        first_linear_in = int(np.ceil(self.f_size/self.stride[0]))
+        self.first_linear = nn.Linear(in_features=first_linear_in, out_features=self.first_linear_out)
+        self.blstm_block = nn.LSTM(input_size=self.first_linear_out,
+                                   hidden_size=self.hidden_size,
+                                   num_layers=2,
+                                   bidirectional=True, 
+                                   batch_first=True)
+        
+        self.last_linear = nn.Linear(in_features=self.hidden_size*2, out_features=self.f_size)
+    
+        
+    def _encoder(self, channels, kernel_size, stride, dilation=1):
+        padding = self._kernel_and_dilation_pad(kernel_size, dilation)
+        return nn.Sequential(nn.Conv2d(in_channels=channels[0],
+                                       out_channels=channels[1],
+                                       kernel_size=kernel_size,
+                                       stride=stride,
+                                       dilation=dilation,
+                                       padding=padding),
+                             nn.InstanceNorm2d(channels[1]))
+        
+        
+    def _stride_pad(self, x, stride):
+        batch, channel, freq, time = x.shape
+        stride_f = self.stride[0]
+        stride_t = self.stride[1]
+        pad_f = int(np.ceil((freq-1)/stride_f)*stride_f) + 1 - freq
+        pad_t = int(np.ceil((time-1)/stride_t)*stride_t) + 1 - time
+        x = torch.cat((x, torch.zeros((batch, channel, freq, pad_t), dtype=x.dtype, device=x.device)),axis=3)
+        x = torch.cat((x, torch.zeros((batch, channel, pad_f, time+pad_t), dtype=x.dtype, device=x.device)), axis=2)
+        return x
+    
+    def _kernel_and_dilation_pad(self, kernel_size, dilation):
+        return [((i + (i-1)*(dilation - 1) - 1) // 2) for i in kernel_size]
+    
+
+    def forward(self,xin, ex1_xin, ex2_xin):
+        batch, freq, time = xin.shape
+       
+        xin = xin.unsqueeze(1)
+        ex1_xin = ex1_xin.unsqueeze(1)
+        ex2_xin = ex2_xin.unsqueeze(1)
+        encoder_in = torch.cat((xin, ex1_xin, ex2_xin), axis=1)
+        encoder_out = self.encoder(self._stride_pad(encoder_in, self.stride))
+        mix_encoder_out = self.mix_encoder(self._stride_pad(encoder_out, self.mix_stride))
+        compressor_out = self.compressor(mix_encoder_out)
+        compressor_out = compressor_out.squeeze(1)#(batch, F, T)
+        compressor_out = compressor_out.permute(0,2,1)
+        first_linear_out = self.first_linear(compressor_out)
+        blstm_out, _ = self.blstm_block(first_linear_out)
+        last = self.last_linear(blstm_out)
+        mask = last.permute(0,2,1)
+        mask = torch.sigmoid(mask)
+        return mask
+    
+if __name__ == '__main__':
+    dnn_cfg = {'dnn_cfg': {'f_size': int(2048/2) + 1, 
+                          'kernel': (9,9), 
+                          'stride': (2,1), 
+                          'channel': (1,10),
+                          'ex1_kernel': (27,3),
+                          'ex1_stride': (2,1),
+                          'ex1_channel': (1,10),
+                          'ex2_kernel': (3,27),
+                          'ex2_stride': (2,1),
+                          'ex2_channel': (1,10),
+                          'mix_kernel': (5,15),
+                          'mix_stride': (1,1),
+                          'mix_channel': (30, 60),
+                          'hidden_size': 400}}
+
+    model = FeatExtractorBlstm_pp(dnn_cfg['dnn_cfg'])
+    params = 0
+    for p in model.parameters():
+        if p.requires_grad:
+            params += p.numel()
+    print('parameters:', params)
