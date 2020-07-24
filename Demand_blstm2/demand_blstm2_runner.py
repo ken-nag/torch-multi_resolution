2f90ab4f4b1a116e31b4d514ef307152e1e6c0ab
ken-nag <jimijeffericking@icloud.com>
2020-07-24 16:20:12 +0900

    feat: T_MAE blstm2

--- Demand_blstm2/demand_blstm2_runner.py
+++ Demand_blstm2/demand_blstm2_runner.py
@@ -5,7 +5,7 @@ sys.path.append('../')
 from models.blstm import BLSTM2
 from data_utils.voice_demand_dataset import VoicebankDemandDataset
 from data_utils.data_loader import FastDataLoader
-from utils.loss import PSA
+from utils.loss import T_MAE
 from utils.visualizer import show_TF_domein_result
 import numpy as np
 from utils.stft_module import STFTModule
@@ -51,7 +51,7 @@ class DemandBLSTM2Runner():
                                                 shuffle=True)
         
         self.model = BLSTM2(cfg['dnn_cfg']).to(self.device)
-        self.criterion = PSA()
+        self.criterion = T_MAE()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
         self.save_path = cfg['save_path']
         self.early_stopping = EarlyStopping(patience=10)
@@ -78,7 +78,7 @@ class DemandBLSTM2Runner():
             est_source = noisy_spec * est_mask[...,None]
             
             if mode == 'train' or mode == 'validation':
-                loss = self.criterion(est_source, clean_spec)
+                loss = self.criterion(est_source, clean_spec, self.stft_module)
                 running_loss += loss.data
                 if mode == 'train':
                     loss.backward()
