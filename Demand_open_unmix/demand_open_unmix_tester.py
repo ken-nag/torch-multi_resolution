425f042f6956e07305ceff985489e3f416d03ac5
ken-nag <jimijeffericking@icloud.com>
2020-07-28 00:07:42 +0900

    fix

--- Demand_open_unmix/demand_open_unmix_tester.py
+++ Demand_open_unmix/demand_open_unmix_tester.py
@@ -42,21 +42,21 @@ class DemandOpenUnmixTester():
         self.si_sdr_list = np.array([])
         self.si_sdr_improve_list = np.array([])
     
-    # def _preprocess(self, noisy):
-    #     with torch.no_grad():
-    #         noisy_spec = self.stft_module.stft(noisy, pad=None)
-    #         noisy_amp_spec = taF.complex_norm(noisy_spec)
-    #         noisy_mag_spec = self.stft_module.to_normalize_mag(noisy_amp_spec)
+    def _preprocess(self, noisy):
+        with torch.no_grad():
+            noisy_spec = self.stft_module.stft(noisy, pad=None)
+            noisy_amp_spec = taF.complex_norm(noisy_spec)
+            noisy_mag_spec = self.stft_module.to_normalize_mag(noisy_amp_spec)
             
-    #         return noisy_mag_spec, noisy_spec
+            return noisy_mag_spec, noisy_spec
     
-    def _postprocess(self, est_spec, mix_spec):
-        est_mag_spec = est_spec.pow(2).sum(-1).pow(1 / 2.0)
-        est_spec = est_spec.cpu().clone().numpy()
-        mix_spec = mix_spec.cpu().clone().numpy()
-        norbert_est = norbert.wiener(est_mag_spec, mix_spec)
-        norbert_est = torch.from_numpy(norbert_est).to(self.dtype).to(self.device)
-        return norbert_est
+    # def _postprocess(self, est_spec, mix_spec):
+    #     est_mag_spec = est_spec.pow(2).sum(-1).pow(1 / 2.0)
+    #     est_spec = est_spec.cpu().clone().numpy()
+    #     mix_spec = mix_spec.cpu().clone().numpy()
+    #     norbert_est = norbert.wiener(est_mag_spec, mix_spec)
+    #     norbert_est = torch.from_numpy(norbert_est).to(self.dtype).to(self.device)
+    #     return norbert_est
         
             
     def test(self, mode='test'):
