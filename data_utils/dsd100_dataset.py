fb957841009092aebb1c70af38eef086d1706de4
ken-nag <jimijeffericking@icloud.com>
2020-08-25 00:48:30 +0900

    fix

--- data_utils/dsd100_dataset.py
+++ data_utils/dsd100_dataset.py
@@ -15,12 +15,16 @@ class DSD100Dataset(torch.utils.data.Dataset):
             self.audio_folder_path = glob.glob('../data/DSD_Dsampled/Sources/Test/*')
             
         if self.folder_type=='Dev':
-            audio_folder_path = glob.glob('../data/DSD_Dsampled/Sources/Dev/*')
-            self.audio_folder_path = audio_folder_path[:self.full_data_num]
+            self.bass_folder_path = glob.glob('../data/DSD_Dev/bass/*')
+            self.drums_folder_path = glob.glob('../data/DSD_Dev/drums/*')
+            self.vocals_folder_path = glob.glob('../data/DSD_Dev/vocals/*')
+            self.other_folder_path = glob.glob('../data/DSD_Dev/other/*')
         
         if self.folder_type=='Validation':
-            audio_folder_path = glob.glob('../data/DSD_Dsampled/Sources/Dev/*')
-            self.audio_folder_path = audio_folder_path[-self.full_data_num:]
+            self.bass_folder_path = glob.glob('../data/DSD_Dev/bass/*')[-self.full_data_num:]
+            self.drums_folder_path = glob.glob('../data/DSD_Dev/drums/*')[-self.full_data_num:]
+            self.vocals_folder_path = glob.glob('../data/DSD_Dev/vocals/*')[-self.full_data_num:]
+            self.other_folder_path = glob.glob('../data/DSD_Dev/other/*')[-self.full_data_num:]
             
         self.sample_len = sample_len
         self.shuffle = shuffle
@@ -29,41 +33,32 @@ class DSD100Dataset(torch.utils.data.Dataset):
         
     def __len__(self):
         return self.data_num
-    
-    # def _crop_per_segment(self, x):
-    #     x_len = x.shape[0]
-    #     batch_size = np.ceil(x_len / self.sample_len).astype(np.int32)
-    #     pad_len = self.sample_len - (x_len % self.sample_len)
-    #     pad_x = torch.zeros(x_len + pad_len, dtype=self.dtype, device=self.device)
-    #     pad_x[:x_len] = x[:]
-    #     return pad_x.reshape(batch_size, self.sample_len)
-    
+      
     def _random_scaling(self, source):
         scale_coeff = random.uniform(0, 1.25) #range 0.25~1.25
         return scale_coeff * source
         
-    def _random_chunking(self, source):
-         start = np.random.randint(len(source) - self.sample_len)
-         return source[start:(start+self.sample_len)]
+    # def _random_chunking(self, source):
+    #      start = np.random.randint(len(source) - self.sample_len)
+    #      return source[start:(start+self.sample_len)]
      
-    def _augmentation(self,source):
-        chunked = self._random_chunking(source)
-        return self._random_scaling(chunked)
+    # def _augmentation(self,source):
+    #     chunked = self._random_chunking(source)
+    #     return self._random_scaling(chunked)
             
     def __getitem__(self, idx):
         if (self.folder_type == 'Dev') or (self.folder_type == 'Validation'):
             if self.augmentation:
-                paths = random.sample(self.audio_folder_path, 4)
-                bass_path = paths[0] + '/bass.wav'
-                drums_path = paths[1] + '/drums.wav'
-                other_path = paths[2] + '/other.wav'
-                vocals_path = paths[3] + '/vocals.wav'
+                bass_path = random.sample(self.bass_folder_path, 1)[-1]
+                drums_path = random.sample(self.drums_folder_path, 1)[-1]
+                other_path = random.sample(self.other_folder_path, 1)[-1]
+                vocals_path = random.sample(self.vocals_folder_path, 1)[-1]
             else:
-                path = self.audio_folder_path[idx]      
-                bass_path = path + '/bass.wav'
-                drums_path = path + '/drums.wav'
-                other_path = path + '/other.wav'
-                vocals_path = path + '/vocals.wav'
+                path = self.audio_folder_path[idx]    
+                bass_path = self.bass_folder_path[idx]
+                drums_path = self.drums_folder_path[idx]
+                other_path = self.other_folder_path[idx]
+                vocals_path = self.vocals_folder_path[idx]
                     
         elif self.folder_type == 'Test':
             path = self.audio_folder_path[idx]      
@@ -84,16 +79,16 @@ class DSD100Dataset(torch.utils.data.Dataset):
         
         if (self.folder_type == 'Dev') or (self.folder_type == 'Validation'):
             if self.augmentation:
-                bass = self._augmentation(bass)
-                drums = self._augmentation(drums)
-                other = self._augmentation(other)
-                vocals = self._augmentation(vocals)
-            else:
-                start = np.random.randint(len(bass) - self.sample_len)
-                bass = bass[start:start+self.sample_len]
-                drums = drums[start:start+self.sample_len]
-                other = other[start:start+self.sample_len]
-                vocals = vocals[start:start+self.sample_len]
+                bass = self._random_scaling(bass)
+                drums = self._random_scaling(drums)
+                other = self._random_scaling(other)
+                vocals = self._random_scaling(vocals)
+                
+            start = np.random.randint(len(bass) - self.sample_len)
+            bass = bass[start:start+self.sample_len]
+            drums = drums[start:start+self.sample_len]
+            other = other[start:start+self.sample_len]
+            vocals = vocals[start:start+self.sample_len]
                 
         mixture = bass + drums + other + vocals
             
